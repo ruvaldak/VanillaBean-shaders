a0a4fdd95b034ecc2d013e4e5ab32fe69a79398f
ruvaldak <truvaldak@gmail.com>
2021-09-05 09:15:32 -0400

    undo last commit

--- shaders/lib/fog.glsl
+++ shaders/lib/fog.glsl
@@ -6,17 +6,12 @@ const int GL_EXP = 2048;
 uniform int fogMode;
 */
 vec4 fog;
-
-if(MC_VERSION >= 11700)
+if(fogMode == GL_EXP) //exponential fog
+    fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density);
+else if (fogMode == GL_LINEAR) //linear fog
     fog.a = clamp((gl_FogFragCoord-gl_Fog.start) * gl_Fog.scale, 0., 1.);
-else {
-    if(fogMode == GL_EXP) //exponential fog
-        fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density);
-    else if (fogMode == GL_LINEAR) //linear fog
-        fog.a = clamp((gl_FogFragCoord-gl_Fog.start) * gl_Fog.scale, 0., 1.);
-    else if (isEyeInWater == 1.0 || isEyeInWater == 2.0)
-        fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density); //denser underwater and underlava fog
-}
+else if (isEyeInWater == 1.0 || isEyeInWater == 2.0)
+    fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density); //denser underwater and underlava fog
 fog.rgb = gl_Fog.color.rgb;
 
 //Apply the fog.
