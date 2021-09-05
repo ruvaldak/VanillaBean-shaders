dcfba2540e3b53636b09e244f081ce0c3290d6ad
ruvaldak <truvaldak@gmail.com>
2021-09-05 09:11:54 -0400

    use linear fog only if using >= 1.17

--- shaders/lib/fog.glsl
+++ shaders/lib/fog.glsl
@@ -6,12 +6,18 @@ const int GL_EXP = 2048;
 uniform int fogMode;
 */
 vec4 fog;
-if(fogMode == GL_EXP) //exponential fog
-	fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density);
-else if (fogMode == GL_LINEAR) //linear fog
-	fog.a = clamp((gl_FogFragCoord-gl_Fog.start) * gl_Fog.scale, 0., 1.);
-else if (isEyeInWater == 1.0 || isEyeInWater == 2.0)
-	fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density); //denser underwater and underlava fog
+
+if(MC_VERSION >= 11700)
+    fog.a = clamp((gl_FogFragCoord-gl_Fog.start) * gl_Fog.scale, 0., 1.);
+else {
+    if(fogMode == GL_EXP) //exponential fog
+        fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density);
+    else if (fogMode == GL_LINEAR) //linear fog
+        fog.a = clamp((gl_FogFragCoord-gl_Fog.start) * gl_Fog.scale, 0., 1.);
+    else if (isEyeInWater == 1.0 || isEyeInWater == 2.0)
+        fog.a = 1.-exp(-gl_FogFragCoord * gl_Fog.density); //denser underwater and underlava fog
+}
 fog.rgb = gl_Fog.color.rgb;
+
 //Apply the fog.
 col.rgb = mix(col.rgb, fog.rgb, fog.a);
